ab0ee37887f88dba69a6a78fb930e05870fd900a
Flaco924 <rom1.dehaese@gmail.com>
2025-06-07 23:50:28 +0200

    ajout fenetre de stream

--- frontend_old/src/App.tsx
+++
@@ -1,7 +0,0 @@
-export default function App() {
-  return (
-    <div className="bg-green-500 text-white p-8 text-2xl rounded-xl shadow-xl text-center mt-20 mx-auto w-fit">
-      ✅ Tailwind fonctionne parfaitement !
-    </div>
-  )
-}
