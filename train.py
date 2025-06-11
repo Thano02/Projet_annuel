38f85c2fed4d69eda61c30f7e66fa41191cc7dd6
ETHAN EL HARRAR <eelharrar@myges.fr>
2025-06-11 14:04:22 +0200

    ajout de data.yaml dans dataset, du fichier entrainement train, et du dossier yolo_training contenant le model dans le dossier weight

---
+++ train.py
@@ -0,0 +1,16 @@
+from ultralytics import YOLO
+
+# Charger le modèle YOLOv8 segmentation pré-entraîné
+model = YOLO("yolov8n-seg.pt")
+
+# Lancer l'entraînement sur ton dataset annoté
+model.train(
+    data="dataset_yolo/data.yaml",
+    epochs=50,
+    imgsz=640,
+    batch=16,
+    project="yolo_training",
+    name="final_model",
+    save=True,
+    classes=6
+)
